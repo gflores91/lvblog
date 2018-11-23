9543c9f0fe0293cf91101129bad1cc2d3f3a9e5d
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2018-11-23 19:46:39 -0300

    Fix HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function Index()
     {
         $noticias = Noticia::latest()->paginate(10);
 
