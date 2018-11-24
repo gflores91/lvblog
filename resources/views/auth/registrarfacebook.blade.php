1f65cecbd8066d9ae899308818f7466a0cb4182b
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2018-11-24 13:15:51 -0300

    Se agrega vista para registro con facebook

--- resources/views/auth/registrarfacebook.blade.php
+++ resources/views/auth/registrarfacebook.blade.php
@@ -3,8 +3,11 @@
 @section('content')
 <div class="row justify-content-center mt-2">
     <div class="col-md-8">
+
         <div class="card">
             <div class="card-header">Registro de usuario</div>
+
+            <img class="card-img-top" src="{{ $user->avatar }}" alt="">
 
             <div class="card-body">
                 <form method="POST" action="{{ route('socialprofile.registrar') }}">
@@ -15,11 +18,26 @@
 
                         <div class="col-md-6">
                             <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                name="name" value="{{ old('name') }}" required autofocus>
+                                name="name" value="{{ $user->name }}" required autofocus readonly>
 
                             @if ($errors->has('name'))
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                name="email" value="{{ $user->email }}" required readonly>
+
+                            @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
                             </span>
                             @endif
                         </div>
@@ -40,22 +58,7 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                name="email" value="{{ old('email') }}" required>
-
-                            @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                            @endif
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
+                    <!-- <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
 
                         <div class="col-md-6">
@@ -68,16 +71,16 @@
                             </span>
                             @endif
                         </div>
-                    </div>
+                    </div> -->
 
-                    <div class="form-group row">
+                    <!-- <div class="form-group row">
                         <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Repita contraseña</label>
 
                         <div class="col-md-6">
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
                                 required>
                         </div>
-                    </div>
+                    </div> -->
 
                     <div class="form-group row mb-0">
                         <div class="col-md-6 offset-md-4">
